2cc8cf231f6fd1720130396e470c1446d531966e
Muhamad Jamil <rezaramdhani461@gmail.com>
2025-11-27 08:24:07 +0700

    show auth user

--- app/components/user/Header.vue
+++ app/components/user/Header.vue
@@ -3,12 +3,10 @@
 		<div class="flex justify-between items-start">
 			<!-- LEFT: LOGO + TEXT -->
 			<div>
-				<!-- LOGO -->
 				<div class="flex items-center gap-3 mb-8">
 					<img src="/images/logo.png" alt="Logo" class="h-10" />
 				</div>
 
-				<!-- TITLE -->
 				<h1 class="text-5xl font-bold text-[#202033] leading-tight">
 					Selamat Datang di Siap
 					<br />Vokasi
@@ -20,13 +18,24 @@
 				<div class="h-10 w-10 rounded-xl bg-white shadow-sm flex items-center justify-center border border-gray-200">
 					<Icon name="lucide:user" class="h-5 w-5 text-gray-500" />
 				</div>
-				<p class="text-gray-500">Hi, <span class="font-semibold text-blue-600">Sean</span></p>
+
+				<p class="text-gray-500">
+					Hi,
+					<span class="font-semibold text-blue-600">
+						{{ auth.user?.name || "User" }}
+					</span>
+				</p>
 			</div>
 
-			<!-- LEFT: DECOR -->
 			<div class="absolute right-0 bottom-0 w-[30%]">
 				<img src="/images/decor.png" alt="Decor Left" class="w-full" />
 			</div>
 		</div>
 	</header>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from "~/stores/auth";
+
+const auth = useAuthStore();
+</script>
