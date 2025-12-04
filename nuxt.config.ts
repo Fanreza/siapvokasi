396a1e23f0e6a23fe3b8ff26c08b097eba730b5a
Muhamad Jamil <rezaramdhani461@gmail.com>
2025-12-04 11:03:19 +0700

    sec

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,16 +58,24 @@ export default defineNuxtConfig({
 
 		headers: {
 			contentSecurityPolicy: {
-				"connect-src": ["'self'", "https://stankom.fanreza.my.id", "https://apistankom.kemnaker.go.id", "https://apisiapvokasi.kemnaker.go.id", "https://www.instagram.com", "https://platform.twitter.com"],
+				"default-src": ["'self'"],
+				"worker-src": ["'self'", "blob:"],
+				"child-src": ["'self'", "blob:"],
 
-				"img-src": ["'self'", "data:", "https://stankom.fanreza.my.id", "https://apistankom.kemnaker.go.id", "https://apisiapvokasi.kemnaker.go.id", "https://storage.googleapis.com", "https://scontent.cdninstagram.com", "https://*.cdninstagram.com", "https://*.fbcdn.net"],
+				"connect-src": ["'self'", "https://stankom.fanreza.my.id", "https://apistankom.kemnaker.go.id", "https://apisiapvokasi.kemnaker.go.id", "https://www.instagram.com", "https://i.instagram.com", "https://graph.instagram.com", "https://platform.twitter.com", "https://maps.googleapis.com"],
 
-				"script-src": ["'self'", "'unsafe-inline'", "https://www.instagram.com", "https://static.cdninstagram.com", "https://platform.twitter.com"],
+				"img-src": ["'self'", "data:", "blob:", "https://apistankom.kemnaker.go.id", "https://apisiapvokasi.kemnaker.go.id", "https://storage.googleapis.com", "https://scontent.cdninstagram.com", "https://*.cdninstagram.com", "https://*.fbcdn.net", "https://maps.gstatic.com", "https://lh3.googleusercontent.com"],
+
+				"script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://www.instagram.com", "https://static.cdninstagram.com", "https://platform.instagram.com", "https://platform.twitter.com", "https://www.google.com", "https://maps.googleapis.com", "https://maps.gstatic.com"],
 
 				"style-src": ["'self'", "'unsafe-inline'", "https://www.instagram.com", "https://static.cdninstagram.com"],
 
-				"frame-src": ["'self'", "https://www.instagram.com", "https://www.google.com", "https://platform.twitter.com"],
+				"frame-src": ["'self'", "https://www.instagram.com", "https://instagram.com", "https://platform.twitter.com", "https://www.google.com", "https://maps.googleapis.com", "https://maps.gstatic.com", "https://www.youtube.com", "https://*.instagram.com"],
 			},
+
+			crossOriginEmbedderPolicy: "unsafe-none",
+			crossOriginResourcePolicy: "cross-origin",
+			crossOriginOpenerPolicy: "unsafe-none",
 		},
 	},
 });
